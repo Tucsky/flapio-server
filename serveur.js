6fc28386d319dbfb7e08ba252f44d9f3249b4142
Tucsky <kevin.rostagni@gmail.com>
2014-03-13 01:46:52 +0100

    Update serveur.js

--- serveur.js
+++ serveur.js
@@ -257,6 +257,11 @@
 		}
 
 		that.listen = function() {
+			io.configure(function () { 
+				io.set("transports", ["xhr-polling"]); 
+				io.set("polling duration", 10); 
+			});
+			
 			io.set('authorization', function (data, callback) {
 				var token = data.query.token ? that.secr.encrypt(data.query.token).substr(0, 16) : null;
 
