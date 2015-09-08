caa3cb0b928c3567ff5540e30d0013da948b3606
Kevin Rostagni <contact@kevinrostagni.fr>
2015-09-08 04:02:47 +0200

    ignore modules, add env salt

--- serveur.js
+++ serveur.js
@@ -685,7 +685,7 @@
 
 
 	function encrypt(text) {
-		var cipher = crypto.createCipher('aes-256-cbc','d6F3Efeq')
+		var cipher = crypto.createCipher('aes-256-cbc', process.env.SALT)
 		var crypted = cipher.update(text,'utf8','hex')
 		crypted += cipher.final('hex');
 		return crypted;
